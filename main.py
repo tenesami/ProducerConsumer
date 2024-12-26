670f603bc0987ad561ec0d9051ea3dd8427660eb
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2024-12-25 18:28:31 -0700

    setting the main

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from producer_consumer import MyThread
+from producer_consumer import SharedCell, Producer, Consumer
 
 def main():
-    MyThread("Ken").start()
+    """Get the number of accessses from the user, creat a 
+    shared cell, and create and start up a producer and a consumer"""
+    access_count = int(input("enter the number of accesses: "))
+    sleep_max = 4 
+    cell = SharedCell()
+    producer = Producer(cell, access_count, sleep_max)
+    consumer = Consumer(cell, access_count, sleep_max)
+
+    print("Starting the threads")
+    producer.start()
+    consumer.start()
+
+ 
 if __name__== "__main__":
     main()
